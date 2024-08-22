9915e8ee720f522c7e2938af697a28beb5fce0c5
erick-costa <erickcosta2509@gmail.com>
2024-08-22 17:04:07 -0300

    implementação de rotas

--- src/app/components/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/components/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -20,8 +20,4 @@ export class CriarPensamentoComponent implements OnInit {
   criarPensamento() {
     alert('Novo pensamento criado');
   }
-
-  cancelar() {
-    alert('Pensamento cancelado!');
-  }
 }
