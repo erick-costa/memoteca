b1bfa9956d2506b8c84617e0b3be75a1f9c5e1a4
erick-costa <erickcosta2509@gmail.com>
2024-08-23 11:50:32 -0300

    criação backend e implementação de interface

--- src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Pensamento } from '../pensamento';
 
 @Component({
   selector: 'app-listar-pensamento',
@@ -6,24 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./listar-pensamento.component.css'],
 })
 export class ListarPensamentoComponent implements OnInit {
-  listaPensamentos = [
-    {
-      conteudo: 'Mando informações para o componente filho',
-      autoria: 'Componente pai',
-      modelo: 'modelo1',
-    },
-    {
-      conteudo: 'Comunicação entre componentes',
-      autoria: 'Angular',
-      modelo: 'modelo2',
-    },
-    {
-      conteudo:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque lacinia tortor id est pellentesque blandit. Cras dictum, dui non laoreet fringilla, ipsum arcu efficitur leo, sed euismod dolor risus sit amet ligula. Nulla facilisi. Nam mattis porttitor sapien in malesuada. Proin consectetur nibh et quam dignissim, sit amet laoreet odio convallis.',
-      autoria: 'Angular',
-      modelo: 'modelo2',
-    },
-  ];
+  listaPensamentos: Pensamento[] = [];
   constructor() {}
 
   ngOnInit(): void {}
