85228d376e048c5240e3e51efc57d313fded4da9
erick-costa <erickcosta2509@gmail.com>
2024-08-23 11:06:22 -0300

    comunicação entre componentes com ngFor

--- src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -3,13 +3,22 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-listar-pensamento',
   templateUrl: './listar-pensamento.component.html',
-  styleUrls: ['./listar-pensamento.component.css']
+  styleUrls: ['./listar-pensamento.component.css'],
 })
 export class ListarPensamentoComponent implements OnInit {
+  listaPensamentos = [
+    {
+      conteudo: 'Mando informações para o componente filho',
+      autoria: 'Componente pai',
+      modelo: 'modelo1',
+    },
+    {
+      conteudo: 'Comunicação entre componentes',
+      autoria: 'Angular',
+      modelo: 'modelo2',
+    },
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
